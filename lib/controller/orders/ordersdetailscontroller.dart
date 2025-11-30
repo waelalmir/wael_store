8542784719edd437c0fe04e3442019383e139813
waelalmir <eng.waelalmir@gmail.com>
2025-11-30 15:46:45 +0300

    Remove comment about product details
    Removed a comment in Arabic regarding product details.

--- lib/controller/orders/ordersdetailscontroller.dart
+++ lib/controller/orders/ordersdetailscontroller.dart
@@ -1,7 +1,6 @@
 import 'package:ecommerce/core/class/statusrequest.dart';
 import 'package:ecommerce/core/class/functions/handlingdatacontroller.dart';
 import 'package:ecommerce/data/datasource/remote/orders/details.dart';
-// تأكد أن هذا الموديل يحتوي على تفاصيل المنتج
 import 'package:ecommerce/data/model/ordersdetailsmodel.dart';
 import 'package:ecommerce/data/model/ordersmodel.dart';
 import 'package:ecommerce/view/screen/orders/mapvieworders.dart';
