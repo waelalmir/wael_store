0cef117334b136384d83a84df0ff8abad0895f10
waelalmir <eng.waelalmir@gmail.com>
2025-11-30 15:32:43 +0300

    Update logincontroller.dart

--- lib/controller/auth/logincontroller.dart
+++ lib/controller/auth/logincontroller.dart
@@ -3,7 +3,6 @@ import 'package:ecommerce/core/constant/routes.dart';
 import 'package:ecommerce/core/class/functions/handlingdatacontroller.dart';
 import 'package:ecommerce/core/services/sevices.dart';
 import 'package:ecommerce/data/datasource/remote/auth/logindata.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -36,7 +35,6 @@ class LoginControllerImp extends LoginController {
       statusRequest = handlingData(response);
       if (StatusRequest.success == statusRequest) {
         if (response['status'] == "success") {
-          // data.addAll(response['data']);
           if (response['data']['users_approve'] == 1) {
             myServices.sharedPrefrences
                 .setString("id", response['data']['users_id'].toString());
@@ -79,21 +77,11 @@ class LoginControllerImp extends LoginController {
 
   @override
   goToSignUp() {
-    // ignore: prefer_const_constructors
     Get.offNamed(AppRoutes.signup);
   }
 
   @override
   void onInit() {
-    FirebaseMessaging.instance.getToken().then((value) {
-      print(
-          "============================================================================================");
-      print(value);
-      print(
-          "============================================================================================");
-      // ignore: unused_local_variable
-      String? token = value;
-    });
     email = TextEditingController();
     password = TextEditingController();
     super.onInit();
