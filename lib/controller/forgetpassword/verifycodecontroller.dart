41877cab994ff3648560bb0f56f5c55e436c0d98
waelalmir <eng.waelalmir@gmail.com>
2025-11-30 15:35:41 +0300

    Update verifycodecontroller.dart

--- lib/controller/forgetpassword/verifycodecontroller.dart
+++ lib/controller/forgetpassword/verifycodecontroller.dart
@@ -11,44 +11,5 @@ abstract class VerifyCodeController extends GetxController {
 }
 
 class VerifyCodeControllerImp extends VerifyCodeController {
-  GlobalKey<FormState> formstate = GlobalKey<FormState>();
-  StatusRequest statusRequest = StatusRequest.none;
-  VerifyCodeData verifyCodeData = VerifyCodeData(Get.find());
-  String? email;
-  late String verifycode;
-  @override
-  checkcode() async {}
-
-  @override
-  gotoresetpassword(String verifyCode) async {
-    if (formstate.currentState?.validate() ?? true) {
-      if (email == null) {
-        Get.defaultDialog(title: "Error", middleText: "Email is missing!");
-        return;
-      }
-      statusRequest = StatusRequest.loading;
-      update();
-
-      var response = await verifyCodeData.postData(email!, verifyCode);
-      print("=============================== Controller $response ");
-      statusRequest = handlingData(response);
-
-      if (StatusRequest.success == statusRequest) {
-        if (response['status'] == "success") {
-          Get.offNamed(AppRoutes.resetPassowrd, arguments: {"email": email});
-        } else {
-          Get.defaultDialog(
-              title: "Warning", middleText: "Verify code is not correct");
-          statusRequest = StatusRequest.failure;
-        }
-      }
-      update();
-    }
-  }
-
-  @override
-  void onInit() {
-    super.onInit();
-    email = Get.arguments['email'];
-  }
+ // Logic
 }
