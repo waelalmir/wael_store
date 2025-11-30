ba20825514ec4b3d3ddf0af7a5861d53e515f10a
waelalmir <eng.waelalmir@gmail.com>
2025-11-30 15:46:13 +0300

    Update orderscontroller.dart

--- lib/controller/orders/orderscontroller.dart
+++ lib/controller/orders/orderscontroller.dart
@@ -22,78 +22,18 @@ class Orderscontroller extends GetxController {
   String? payMethod;
   late String addressid;
 
-  viewOrders() async {
-    statusRequest = StatusRequest.loading;
+  viewOrders() async {}
 
-    update();
-    var response = await ordersPendingData
-        .getData(myServices.sharedPrefrences.getString("id")!);
-    print("=============================== Controller $response ");
-    statusRequest = handlingData(response);
-    if (StatusRequest.success == statusRequest) {
-      if (response != null && response['data'] != null) {
-        data.clear();
+  deleteOrders(String orderid) async {}
 
-        List dataresponse = response['data'];
-        // Map dataresponsecountprice = response['countprice'];
-        data.addAll(dataresponse.map((e) => OrdersModel.fromJson(e)));
-        animated = List<bool>.filled(data.length, false);
-      }
-    } else {
-      statusRequest = StatusRequest.failure;
-      Get.snackbar("Oops", "There was no orders yet");
-    }
-    update();
-  }
-
-  deleteOrders(String orderid) async {
-    data.clear();
-    statusRequest = StatusRequest.loading;
-
-    update();
-    var response = await deleteOrdersData.deleteData(orderid);
-    print("=============================== Controller $response ");
-    statusRequest = handlingData(response);
-    if (StatusRequest.success == statusRequest) {
-      refrehOrder();
-    } else {
-      statusRequest = StatusRequest.failure;
-      Get.snackbar("Oops", "orders not delete it");
-    }
-    update();
-  }
-
-  ratingOrder(String ordersid, double rating, String comment) async {
-    statusRequest = StatusRequest.loading;
-
-    update();
-    var response = await ordersPendingData.ratingOrdersData(
-        ordersid, rating.toString(), comment);
-    print("=============================== Controller $response ");
-    statusRequest = handlingData(response);
-    if (StatusRequest.success == statusRequest) {
-      refrehOrder(); // if (response != null && response['data'] != null) {
-      //   data.clear();
-
-      //   List dataresponse = response['data'];
-      //   // Map dataresponsecountprice = response['countprice'];
-      //   data.addAll(dataresponse.map((e) => OrdersModel.fromJson(e)));
-      // }
-    } else {
-      statusRequest = StatusRequest.failure;
-      Get.snackbar("Oops", "There order faild rating");
-    }
-    update();
-  }
+  ratingOrder(String ordersid, double rating, String comment) async {}
 
   String getPayMethodText(String payMethodValue) {
-    // التحقق من القيمة المدخلة وتطبيق المنطق
     if (payMethodValue == "0") {
       return "Cash";
     } else if (payMethodValue == "1") {
       return "Card";
     } else {
-      // قيمة افتراضية في حال كانت القيمة غير 0 أو 1
       return "Unknown";
     }
   }
@@ -102,9 +42,7 @@ class Orderscontroller extends GetxController {
     viewOrders();
   }
 
-  // goToOrdersDetails() {
-  //   Get.toNamed(AppRoutes.ordersDetails, arguments: {"ordersmodel": listdata});
-  // }
+
   String printOrderStatus(String val) {
     if (val == "0") {
       return "Pending Approval";
