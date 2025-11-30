10cd7a18e583527b134e98090e3b06c9630cc2be
waelalmir <eng.waelalmir@gmail.com>
2025-11-30 15:51:17 +0300

    Update tracking_controller.dart

--- lib/controller/orders/tracking_controller.dart
+++ lib/controller/orders/tracking_controller.dart
@@ -7,11 +7,9 @@ import 'package:latlong2/latlong.dart';
 class TrackingController extends GetxController {
   StreamSubscription<Position>? positionStream;
   LatLng currentDeliveryLocation = const LatLng(0, 0);
-  // 🆕 2. دالة لطلب الأذونات قبل بدء التتبع
   Future<void> _handlePermission() async {
     bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // يمكنك عرض رسالة خطأ أو طلب فتح إعدادات الموقع
       return;
     }
 
@@ -20,27 +18,13 @@ class TrackingController extends GetxController {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied ||
           permission == LocationPermission.deniedForever) {
-        // يمكنك عرض رسالة خطأ بأن الأذونات مرفوضة
         return;
       }
     }
   }
 
   getCurrentLocation() async {
-    await _handlePermission();
-
-    positionStream = Geolocator.getPositionStream().listen(
-      (Position? position) {
-        if (position != null) {
-          // 🆕 تحديث الموقع الحي للدليفري
-          currentDeliveryLocation =
-              LatLng(position.latitude, position.longitude);
-          print(
-              "Delivery Location Updated: ${currentDeliveryLocation.latitude} , ${currentDeliveryLocation.longitude}");
-          update(); // 🆕 إرسال إشعار تحديث لكل GetBuilder يستمع إلى هذا المتحكم
-        }
-      },
-    );
+    // logic
   }
 
   @override
